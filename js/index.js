7a6c00efa882b2b300f555125343768461ce2d97
mycak <mycak@wp.pl>
2020-06-27 20:00:54 +0200

    next commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-
+// Changing opacity of Navigation
 const changeNav = () => {
     const slideInAt = window.scrollY;
     if (slideInAt > 100) {
@@ -8,6 +8,37 @@ const changeNav = () => {
         base.nav.classList.remove('scrolled')
     }
 }
+
+// Adding Shadow to navigation dependent to section
+
+
+
+
+const changeHash = (e) => {
+  base.sections.forEach(section => {
+    const slideInAt = window.scrollY + window.innerHeight/3;
+    const topSection = section.offsetTop;
+    const bottomSection = section.offsetTop + section.offsetHeight;
+    
+    if (topSection < slideInAt && slideInAt < bottomSection ) {
+      window.location.hash = section.id;
+    }
+  })
+
+
+  //adding shadow
+  const divs = document.querySelectorAll('.navbar__shadow > a');
+
+  divs.forEach(div => {
+    if (div.href === document.URL) {
+      div.classList.add('active--shadow');
+    } else {
+      div.classList.remove('active--shadow');
+    }
+  })
+}
+
+
 
 const goToHash = () => {
   document.getElementById('zawory').scrollIntoView({
@@ -34,6 +65,7 @@ const goToHash4 = () => {
 
 
 const base = {
+    sections: document.querySelectorAll('section'),
     nav: document.querySelector('.navbar'),
     arrow: document.querySelector('.arrow'),
     arrow2: document.querySelector('.arrow--2'),
@@ -63,7 +95,8 @@ base.imgBoxes.forEach(box => box.addEventListener('click', e => {
   })
 )
 
-function debounce(func, wait = 10, immediate = true) {
+
+function debounce(func, wait = 15, immediate = true) {
     var timeout;
     return function() {
       var context = this, args = arguments;
@@ -79,6 +112,7 @@ function debounce(func, wait = 10, immediate = true) {
   };
 
   window.addEventListener('scroll', debounce(changeNav));
+  window.addEventListener('scroll', debounce(changeHash));
 
   base.arrow.addEventListener('click', goToHash);
   base.arrow2.addEventListener('click', goToHash2);
